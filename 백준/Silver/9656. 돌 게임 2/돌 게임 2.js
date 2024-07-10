fbe4abf65f0f876e32732bdd8fcb749342aebacc
Bae Geon Ho <tarae7617@naver.com>
2024-07-11 02:44:14 +0900

    [Silver V] Title: 돌 게임 2, Time: 168 ms, Memory: 9376 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.js
@@ -0,0 +1,5 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(+input % 2 === 0 ? 'SK' : 'CY');
