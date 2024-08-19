15ea9c93ddb69de1ee330a84d456ddd983c02048
Bae Geon Ho <tarae7617@naver.com>
2024-08-19 21:02:19 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.js
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.js
@@ -2,19 +2,12 @@ let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim();
 
-const reverse = input.split("").reverse().join("");
-if (input.includes(reverse)) {
-  console.log(reverse.length);
-  return;
-}
-
 const len = input.length;
-for (let i = len - 1; i >= 0; i--) {
-  const r = reverse.substring(i, len);
+const reverse = input.split("").reverse().join("");
 
-  const compare = input + r;
-  if (compare.includes(reverse)) {
-    console.log(compare.length);
-    return;
+for (let i = len; i >= 0; i--) {
+  if ((input + reverse.substring(i, len)).endsWith(reverse)) {  
+    console.log(len * 2 - i);
+    break;
   }
 }
