bd63609a4e0e48912381337fab4c33944e383355
Bae Geon Ho <tarae7617@naver.com>
2024-09-06 15:36:25 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 372 ms, Memory: 21928 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const [N, K] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const visit = Array(10001).fill(0);
+
+const bfs = (num, cnt) => {
+  const queue = [[num, cnt]];
+  visit[num] = 1;
+
+  while (queue.length) {
+    const [n, c] = queue.shift();
+
+    if (n === K) return c;
+
+    for (const next of [n * 2, n - 1, n + 1]) {
+      if (!visit[next] && next <= 100000 && next >= 0) {
+        visit[next] = 1;
+        next === n * 2 ? queue.push([next, c]) : queue.push([next, c + 1]);
+      }
+    }
+  }
+};
+
+console.log(bfs(N, 0));
