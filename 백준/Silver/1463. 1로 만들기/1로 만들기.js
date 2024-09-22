9d597b4d68fbc72ccddf1866f5c202388549da88
Bae Geon Ho <tarae7617@naver.com>
2024-09-23 08:49:49 +0900

    [Silver III] Title: 1로 만들기, Time: 208 ms, Memory: 44560 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,23 +1,15 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split(" ");
+const input = +fs.readFileSync(filePath).toString().trim();
 
-let num = +input[0];
+const dp = [0, 0, 1, 1];
 
-const dp = Array(num + 1).fill(Infinity);
-[dp[1], dp[2], dp[3]] = [0, 1, 1];
+for (let i = 4; i <= input; i++) {
+  const n1 = i % 3 === 0 ? dp[i / 3] + 1 : Infinity;
+  const n2 = i % 2 === 0 ? dp[i / 2] + 1 : Infinity;
+  const n3 = dp[i - 1] + 1;
 
-const solution = (num) => {
-  for (let i = 4; i <= num; i++) {
-    if (i % 3 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-    }
-    if (i % 2 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-    }
-    dp[i] = Math.min(dp[i], dp[i - 1] + 1);
-  }
-  return dp[num];
-};
+  dp[i] = Math.min(n1, n2, n3);
+}
 
-console.log(solution(num));
+console.log(dp[input]);
