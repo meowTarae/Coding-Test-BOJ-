fff3cb319c44ae7f8f0d29977adf341feed47fa4
Bae Geon Ho <tarae7617@naver.com>
2025-03-18 22:53:04 +0900

    [level 2] Title: 땅따먹기, Time: 46.91 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,12 @@
+function solution(land) {
+  const len = land.length;
+  
+  for (let i = 1; i < len; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+  }
+  
+  return Math.max(...land[len - 1]);
+}
