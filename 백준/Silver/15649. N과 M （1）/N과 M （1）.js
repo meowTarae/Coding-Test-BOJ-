dcf22eba46d2bb9ff3a944363d214d75829cabb4
Bae Geon Ho <tarae7617@naver.com>
2025-03-26 15:59:43 +0900

    [Silver III] Title: N과 M (1), Time: 1728 ms, Memory: 13808 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,29 +1,25 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const [N, M] = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split(" ")
-  .map(Number);
+let [N, M] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
 
-const arr = Array(M).fill("");
 const visited = Array(N + 1).fill(false);
+const result = [];
 
-const dfs = (n) => {
-  if (n === M) {
-    console.log(...arr); 
+const dfs = (arr) => {
+  if (arr.length === M) {
+    console.log(...arr);
     return;
   }
 
   for (let i = 1; i <= N; i++) {
     if (visited[i]) continue;
 
-    arr[n] = i; 
     visited[i] = true;
-    dfs(n + 1); 
-    visited[i] = false; 
+    arr.push(i);
+    dfs(arr);
+    visited[i] = false;
+    arr.pop();
   }
 };
 
-dfs(0); 
+dfs([]);
