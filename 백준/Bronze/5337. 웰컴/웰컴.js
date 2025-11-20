f84af7451b7ab1a26db74e38b6ac2a0ec7281472
Bae Geon Ho <tarae7617@naver.com>
2025-11-20 21:42:16 +0900

    [Bronze V] Title: 웰컴, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.js
@@ -0,0 +1,4 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+// let input = fs.readFileSync(filePath).toString().trim().split("\n");
+console.log(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
